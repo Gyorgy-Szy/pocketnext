8c5db037ce48a7a15e3b9e0cee783dd8266c6ffc
György Szy <szygyuri@gmail.com>
2023-09-09 12:19:06 +0200

    pocketbase package added, using new PocketBase() to fetch.

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
+import PocketBase from "pocketbase";
 
 async function getNotes() {
-  const res = await fetch(
-    `http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30`,
-    { cache: "no-store" }
-  );
-  const data = await res.json();
-  return data?.items as any[];
+  const db = new PocketBase("http://127.0.0.1:8090");
+  const data = await db.collection("notes").getFullList();
+
+  // const res = await fetch(
+  //   `http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30`,
+  //   { cache: "no-store" }
+  // );
+  // const data = await res.json();
+
+  console.log("getList returned:", data);
+
+  return data as any[];
 }
 
 export default async function NotesPage() {
   const notes = await getNotes();
-  console.log("got notes:", notes);
+  // console.log("got notes:", notes);
   return (
     <div>
       <h1>NOTES</h1>
